9f2684cf3b6b9ffd24e08adccc185dca2996c119
kartik <anshgarg06@gmail.com>
2021-10-01 10:07:13 +0530

    Added code for plotting age and netWorth Distribution

---
+++ code/analyse.py
@@ -0,0 +1,82 @@
+import numpy as np
+import copy as copy
+from matplotlib import pyplot as plt
+
+
+# code explanation
+## to be uncommented
+
+def plotAgeDistro(ageData):
+    ageData = ageData.to_numpy()
+    
+    #determining minimum and maximum age values in the dataset given
+    minAge = 1000
+    maxAge = 0
+    for age in ageData:
+        if age<minAge:
+            minAge = age
+        if age>maxAge:
+            maxAge = age
+    
+    #making brakets for histogram 
+    ageBrackets = []
+    minLim = minAge//5
+    maxLim = 0
+    if maxAge%5 == 0:
+        maxLim = (maxAge//5) + 1
+    else:
+        maxLim = (maxAge//5) + 2
+    for i in range(minLim,maxLim):
+        ageBrackets.append(i*5)
+    
+    # Creating histogram
+    ##fig, ax = plt.subplots(figsize =(10, 7))
+    ##ax.hist(ageData, ageBrackets)
+ 
+    # Show plot
+    ##plt.show()
+
+
+def plotValueDistro(valData):
+    #Comverting Millions and Thousands to actual decimals
+    convertedValData = []
+    exceptIndex = []
+    sumC = 0
+    numC = 0
+    for i in range(len(valData)):
+        value = copy.deepcopy(valData[i])
+        last = value[len(value)-1]
+        value = value[1:len(value)-1]
+        if len(value) == 0:
+            value = '0'
+            exceptIndex.append(i)
+        else:
+            numC = numC + 1
+        value = float(value)
+        if last == 'M':
+            value = value*1e6
+        else:
+            value = value*1e3
+        value = int(value)
+        sumC = sumC + value;
+        convertedValData.append(value)
+
+    #Handling noise/errors by assigning the average value
+    sumC = sumC // numC
+    for i in exceptIndex:
+        convertedValData[i] = sumC
+    
+    # Creating histogram
+    ## fig, ax = plt.subplots(figsize =(10, 7))
+    ## ax.ticklabel_format(useOffset=False,style='plain')
+    ## arr = ax.hist(convertedValData, bins = 10)
+    ## for i in range(10):
+    ##     plt.text(arr[1][i],arr[0][i],str(arr[0][i]))
+
+    # Show plot
+    ## plt.show()
+    # ax.ticklabel_format(useOffset=True)
+
+def analyse(data):
+    plotAgeDistro(data['Age'])
+    plotValueDistro(data['Value'])
--- code/main.py
+++ code/main.py
@@ -1,4 +1,9 @@
+from analyse import analyse
 import pandas as pd
 # df.iloc[2:4,1]
 PATH = "../data/football_data.csv"
 data = pd.read_csv(PATH)
+
+
+#############  PERFORMING ANALYSIS #############
+analyse(data)
